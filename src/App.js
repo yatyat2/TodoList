35ca04a207b09c286ccc386ebd018c43bbd50a58
yatyat2 <xowls2287@naver.com>
2018-08-03 16:47:05 +0900

    Form 기능 구현

--- src/App.js
+++ src/App.js
@@ -14,9 +14,46 @@ class App extends Component {
       { id: 2, text: "리액트 소개", checked: false }
     ]
   };
+
+  handleChange = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: "",
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  };
+
+  handleKeyPress = e => {
+    if (e.key === "Enter") {
+      this.handleCreate();
+    }
+  };
+
   render() {
+    const { input } = this.state;
+    const { handleChange, handleCreate, handleKeyPress } = this;
+
     return (
-      <TodoListTemplate form={<Form />}>
+      <TodoListTemplate
+        form={
+          <Form
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate}
+          />
+        }
+      >
         <TodoItemList />
       </TodoListTemplate>
     );
