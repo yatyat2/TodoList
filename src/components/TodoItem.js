e1b0560a4e78bc0d4251b39b1152bd691a0728d5
yatyat2 <xowls2287@naver.com>
2018-08-03 18:23:35 +0900

    TodoItem, TodoItemList 최적화

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import "./TodoItem.css";
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps,nextState){
+    return this.props.checked !== nextProps.checked;
+  }
+  
   render() {
     const { text, checked, id, onToggle, onRemove } = this.props;
+    console.log(id);
 
     return (
       <div className="todo-item" onClick={() => onToggle(id)}>
