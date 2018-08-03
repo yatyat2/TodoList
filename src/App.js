d8364e820aac36369c922121157d846579f1cc4a
yatyat2 <xowls2287@naver.com>
2018-08-03 16:24:52 +0900

    App.js에 초기 state 정의

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import TodoListTemplate from './components/TodoListTemplate.js'
-import React, {Component} from 'react';
-import Form from './components/Form'
-import TodoItemList from './components/TodoItemList'
-class App extends Component{
-  render(){
+import TodoListTemplate from "./components/TodoListTemplate.js";
+import React, { Component } from "react";
+import Form from "./components/Form";
+import TodoItemList from "./components/TodoItemList";
+
+class App extends Component {
+  id = 3;
+
+  state = {
+    input: "",
+    todos: [
+      { id: 0, text: "리액트 소개", checked: false },
+      { id: 1, text: "리액트 소개", checked: true },
+      { id: 2, text: "리액트 소개", checked: false }
+    ]
+  };
+  render() {
     return (
-      <TodoListTemplate form={<Form/>}>
-        <TodoItemList/>
+      <TodoListTemplate form={<Form />}>
+        <TodoItemList />
       </TodoListTemplate>
-    )
+    );
   }
 }
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,23 +1,27 @@
-import React, {Component} from 'react';
-import './TodoItem.css';
+import React, { Component } from "react";
+import "./TodoItem.css";
 
-class TodoItem extends Component{
-    render(){
-        const { text, checked, id, onToggle, onRemove} = this.props;
+class TodoItem extends Component {
+  render() {
+    const { text, checked, id, onToggle, onRemove } = this.props;
 
-        return (
-            <div className="todo-item" onClick={()=>onToggle(id)}>
-            <div className="remove" onClick={(e)=>{
-                e.stopPropagation();
-            }}>&times;</div>
+    return (
+      <div className="todo-item" onClick={() => onToggle(id)}>
+        <div
+          className="remove"
+          onClick={e => {
+            e.stopPropagation();
+          }}
+        >
+          &times;
+        </div>
 
-            <div className={`todo-text ${checked && 'checked'}`}>
-                <div>{text}</div>
-            </div>
-            
-            </div>
-        )
-    }
+        <div className={`todo-text ${checked && "checked"}`}>
+          <div>{text}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TodoItem;
