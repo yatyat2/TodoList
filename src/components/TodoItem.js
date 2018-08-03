ac25d08135450ee2ab365bf94419c71bffb046d8
yatyat2 <xowls2287@naver.com>
2018-08-03 14:05:02 +0900

    TodoItemjs 파일 추가

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './TodoItem.css';
+
+class TodoItem extends Component{
+    render(){
+        const { text, checked, id, onToggle, onRemove} = this.props;
+
+        return (
+            <div className="todo-item" onClick={()=>onToggle(id)}>
+            <div className="remove" onClick={(e)=>{
+                e.stopPropagation();
+            }}>&times;</div>
+
+            <div className={`todo-text ${checked && 'checked'}`}>
+                <div>{text}</div>
+            </div>
+            
+            </div>
+        )
+    }
+}
+
+export default TodoItem;
