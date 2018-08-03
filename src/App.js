50267b8cadec945ec2a975c2d50ee14e3ca60d2e
yatyat2 <xowls2287@naver.com>
2018-08-03 13:24:24 +0900

    From component만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import TodoListTemplate from './components/TodoListTemplate.js'
 import React, {Component} from 'react';
+import Form from './components/Form'
 
 class App extends Component{
   render(){
     return (
-      <TodoListTemplate>
+      <TodoListTemplate form={<Form/>}>
         템플릿완성
       </TodoListTemplate>
     )
