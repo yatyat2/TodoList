6f96c574efd6b7f3b1b4d68bd25eba92e89db598
yatyat2 <xowls2287@naver.com>
2018-08-03 17:15:18 +0900

    체크하기, 풀기

--- src/App.js
+++ src/App.js
@@ -39,9 +39,26 @@ class App extends Component {
     }
   };
 
+  handleToggle = id => {
+    const { todos } = this.state;
+
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index];
+    const nextTodos = [...todos];
+
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  };
+
   render() {
-    const { input,todos} = this.state;
-    const { handleChange, handleCreate, handleKeyPress } = this;
+    const { input, todos } = this.state;
+    const { handleChange, handleCreate, handleKeyPress, handleToggle } = this;
 
     return (
       <TodoListTemplate
@@ -54,7 +71,7 @@ class App extends Component {
           />
         }
       >
-        <TodoItemList todos={todos}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} />
       </TodoListTemplate>
     );
   }
