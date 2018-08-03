1e25a94fff9c9bfc54f43b1f93f28ab26c3da2ad
yatyat2 <xowls2287@naver.com>
2018-08-03 13:30:32 +0900

    TodoItemList.js 파일 완료

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class TodoItemList extends Component {
+  render() {
+    const { todos, onToggle, onRemeve } = this.props;
+
+    return (
+        <div>
+
+        </div>
+    )
+  }
+}
+
+export default TodoItemList
